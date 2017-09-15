146abcbc271be0054581fc20127939d177a1b0bf
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-15 19:42:34 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dyscord-plugin',
+    version='0.1',
+    description='The base Dyscord Plugin',
+    author='Dyscord',
+    url='http://github.com/dyscord',
+    packages=find_packages()
+)
