947a65672ee32803d420524fe57c1707878b688e
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-24 17:05:39 -0400

    Version bump to '0.2.1'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dyscord-plugin',
-    version='0.2',
+    version='0.2.1',
     description='The base Dyscord Plugin',
     author='Dyscord',
     url='http://github.com/dyscord',
